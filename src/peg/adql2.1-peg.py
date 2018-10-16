40f39a4444e58864a6ac4b8a8e2f6a4e26a171e6
jon <jon@iparizar>
2018-10-16 18:42:51 +0200

    Very initial initial version of PEG parsing

---
+++ src/peg/adql2.1-peg.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##################################################################
+# pegparse.py:                                                   #
+#     Python Script which parses the ADQL 2.1 standard using PEG #
+##################################################################
+
+import sys
+from arpeggio.peg import ParserPEG
+from xml.etree import ElementTree as etree
+
+def assert_valid(parser, query, uuid):
+    try:
+	parser.parse(query)
+    except Exception, msg:
+        sys.stderr.write("'%s' doesn't parse but should:\n%s\n\n"%(query, msg))
+
+
+def assert_invalid(parser, query, uuid):
+    try:
+ 	parser.parse(query)
+    except Exception, msg:
+	sys.stderr.write("'%s' raises non-parse exception:\n%s\n\n"%(query, msg))
+
+def run_test(query_el, parser):
+    adql = query_el.find("adql")
+    if adql.get("valid")=="true":
+        assert_valid(parser, adql.text.upper(), query_el.get("uuid"))
+    else:
+        assert_invalid(parser, adql.text.upper(), query_el.get("uuid"))
+
+def test_file(file_name, parser):
+    with open(file_name) as f:
+        for ev, el in etree.iterparse(f):
+	    if el.tag=="query":
+	        run_test(el, parser)
+
+if __name__=="__main__":
+    with open("adql21-gm.peg", "r") as grammar_file:
+        adql_grammar = grammar_file.read()
+        print "CREATING GRAMMAR.."
+        parser = ParserPEG(adql_grammar, 'query', ignore_case=True, skipws=False, debug=False, reduce_tree=True)
+        print "GRAMMAR CREATED"
+        #for f in ["geometry.xml", "regressionlike.xml", "setexpressions.xml", "simpleunit.xml", "subqueries.xml"]:
+        for f in ["simpleunit.xml"]:
+            test_file(f, parser)
+
