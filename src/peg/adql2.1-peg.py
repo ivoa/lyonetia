aa8563cb4843542c38093fa5683772d40c0d59eb
jontxu <jonjc22@gmail.com>
2018-10-17 16:30:02 +0200

    Improvements: - More PEG rules which increase test success rate. - Prettyfied the python code by using a global parser variable. - Small bash script which removes the PEG rules in the debug output (a better with filtering must be done)

--- src/peg/adql2.1-peg.py
+++ src/peg/adql2.1-peg.py
@@ -1,48 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-##################################################################
-# pegparse.py:                                                   #
-#     Python Script which parses the ADQL 2.1 standard using PEG #
-##################################################################
+####################################################################
+# adql2.1-peg.py                                                   #
+#     Python script which supports the ADQL 2.1 standard in PEG    #
+#     format (located at adql2.1.peg). This file should eventually #
+#     be moved to another folder in order to not clash with the    #
+#     project structure (preferably src/gavo) and be renamed,      #
+#     e.g., testpeg.py                                             #                       
+#     Requirements:                                                #
+#         Arpeggio for PEG support.                                #
+#         <http://www.igordejanovic.net/Arpeggio/stable>           #
+####################################################################
+# Authors:                                                         #
+#     Jon Juaristi Campillo (ARI, ZAH, Universität Heidelberg)     #
+#     <mailto:juaristi@uni-heidelberg.de>                          #
+####################################################################
 
 import sys
+from arpeggio import NoMatch
 from arpeggio.peg import ParserPEG
 from xml.etree import ElementTree as etree
 
-def assert_valid(parser, query, uuid):
+global parser
+
+def assert_valid(query, uuid):
     try:
 	parser.parse(query)
     except Exception, msg:
-        sys.stderr.write("'%s' doesn't parse but should:\n%s\n\n"%(query, msg))
+        sys.stderr.write("Doesn't parse but should:\n%s\n\n"%( msg))
+    else:
+        sys.stderr.write("Successfully analysed as a valid query\n")
 
-
-def assert_invalid(parser, query, uuid):
+def assert_invalid(query, uuid):
     try:
  	parser.parse(query)
+    except NoMatch:
+        sys.stderr.write("Can't parse invalid query\n")
+        pass
     except Exception, msg:
-	sys.stderr.write("'%s' raises non-parse exception:\n%s\n\n"%(query, msg))
-
-def run_test(query_el, parser):
-    adql = query_el.find("adql")
-    if adql.get("valid")=="true":
-        assert_valid(parser, adql.text.upper(), query_el.get("uuid"))
+	sys.stderr.write("Raises non-parse exception:\n%s\n\n"%( msg))
     else:
-        assert_invalid(parser, adql.text.upper(), query_el.get("uuid"))
+        sys.stderr.write("Parses but shouldn't\n")
 
-def test_file(file_name, parser):
+def run_test(query_el):
+    adql = query_el.find("adql")
+    sys.stderr.write("%s" % (adql.text.upper()))
+    if adql.get("valid")=="true":
+        assert_valid(adql.text.upper(), query_el.get("uuid"))
+    else:
+        assert_invalid(adql.text.upper(), query_el.get("uuid"))
+
+def test_file(file_name):
     with open(file_name) as f:
         for ev, el in etree.iterparse(f):
 	    if el.tag=="query":
-	        run_test(el, parser)
+	        run_test(el)
 
 if __name__=="__main__":
-    with open("adql21-gm.peg", "r") as grammar_file:
+    with open("adql2.1.peg", "r") as grammar_file:
         adql_grammar = grammar_file.read()
-        print "CREATING GRAMMAR.."
-        parser = ParserPEG(adql_grammar, 'query', ignore_case=True, skipws=False, debug=False, reduce_tree=True)
+        print "CREATING GRAMMAR..."
+        parser = ParserPEG(adql_grammar, 'query', ignore_case=True, skipws=False, debug=True, reduce_tree=True)
         print "GRAMMAR CREATED"
         #for f in ["geometry.xml", "regressionlike.xml", "setexpressions.xml", "simpleunit.xml", "subqueries.xml"]:
         for f in ["simpleunit.xml"]:
-            test_file(f, parser)
+            test_file(f)
 
---
+++ src/peg/clean-dbg-output.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -i '/GRAMMAR CREATED/p; /CREATING GRAMMAR/,/GRAMMAR CREATED/d' output.txt
+#Matching rule query=Sequence at position 0
