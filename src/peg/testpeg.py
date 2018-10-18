adfef015e26d7baf9d92b9178379be064f133638
jontxu <jonjc22@gmail.com>
2018-10-18 17:45:30 +0200

    Python file renamed and little of its content changed (variable names, forthe most part); extension on the script removed; grammar supports more ADQL features in theory, albeit not yet in practice

--- src/peg/testpeg.py
+++ src/peg/testpeg.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 ####################################################################
-# adql2.1-peg.py                                                   #
+# testpeg.py                                                       #
 #     Python script which supports the ADQL 2.1 standard in PEG    #
 #     format (located at adql2.1.peg). This file should eventually #
 #     be moved to another folder in order to not clash with the    #
-#     project structure (preferably src/gavo) and be renamed,      #
-#     e.g., testpeg.py                                             #                       
+#     project structure (preferably src/gavo)                      #
 #     Requirements:                                                #
 #         Arpeggio for PEG support.                                #
 #         <http://www.igordejanovic.net/Arpeggio/stable>           #
@@ -25,7 +24,6 @@ from xml.etree import ElementTree as etree
 global parser
 
 def assert_valid(query, uuid):
-    print query
     try:
 	parser.parse(query)
     except Exception, msg:
@@ -34,7 +32,6 @@ def assert_valid(query, uuid):
         print "Successfully analysed as a valid query"
 
 def assert_invalid(query, uuid):
-    print query
     try:
  	parser.parse(query)
     except NoMatch:
@@ -47,6 +44,7 @@ def assert_invalid(query, uuid):
 
 def run_test(query_el):
     adql = query_el.find("adql")
+    print adql.text
     if adql.get("valid")=="true":
         assert_valid(adql.text.upper(), query_el.get("uuid"))
     else:
@@ -59,11 +57,11 @@ def test_file(file_name):
 	        run_test(el)
 
 if __name__=="__main__":
-    with open("adql2.1.peg", "r") as grammar_file:
-        adql_grammar = grammar_file.read()
+    with open("adql2.1.peg", "r") as adql_peg_file:
+        adql_peg = adql_peg_file.read()
         print "CREATING GRAMMAR..."
-        parser = ParserPEG(adql_grammar, 'query', ignore_case=True, skipws=False, debug=True, reduce_tree=True)
-        print "GRAMMAR CREATED"
+        parser = ParserPEG(adql_peg, 'query', ignore_case=True, skipws=False, debug=True, reduce_tree=True)
+        print "GRAMMAR CREATED\n"
         #for f in ["geometry.xml", "regressionlike.xml", "setexpressions.xml", "simpleunit.xml", "subqueries.xml"]:
         for f in ["simpleunit.xml"]:
             test_file(f)
