d8e1b781028071ef590249bf678cf2551c41df20
Markus Demleitner <m@tfiu.de>
2021-12-10 13:43:36 +0100

    Adding tests for coalesce.
    Unrelatedly, porting the two pieces of python to python3, and adding uuids
    to additionaltests; that's just to let me run the tests.

--- src/adql/gavo/testdachs.py
+++ src/adql/gavo/testdachs.py
@@ -9,36 +9,39 @@ def parse(statement, grammar=adql.getRawGrammar()[1]):
 	return grammar.parseString(statement, parseAll=True)
 
 
-def assert_valid(query, uuid):
+def assert_valid(query, src_name, uuid):
 	try:
 		parse(query)
-	except Exception, msg:
-		sys.stderr.write("%s doesn't parse but should: %s\n"%(uuid, msg))
+	except Exception as msg:
+		sys.stderr.write("%s#%s doesn't parse but should: %s\n"%(
+		    src_name, uuid, msg))
 
 
-def assert_invalid(query, uuid):
+def assert_invalid(query, src_name, uuid):
 	try:
 		parse(query)
 	except (adql.ParseException, adql.ParseSyntaxException):
 		pass
-	except Exception, msg:
-		sys.stderr.write("%s raises non-parse exception: %s\n"%(uuid, msg))
+	except Exception as msg:
+		sys.stderr.write("%s#%s raises non-parse exception: %s\n"%(
+		    src_name, uuid, msg))
 	else:
-		sys.stderr.write("%s parses but shouldn't.\n"%uuid)
+		sys.stderr.write("%s#%s parses but shouldn't.\n"%(
+		    src_name, uuid))
 
 
-def run_test(query_el):
+def run_test(query_el, file_name):
 	adql = query_el.find("adql")
 	if adql.get("valid")=="true":
-		assert_valid(adql.text, query_el.get("uuid"))
+		assert_valid(adql.text, file_name, query_el.get("uuid"))
 	else:
-		assert_invalid(adql.text, query_el.get("uuid"))
+		assert_invalid(adql.text, file_name, query_el.get("uuid"))
 
 def test_file(file_name):
 	with open(file_name) as f:
 		for ev, el in etree.iterparse(f):
 			if el.tag=="query":
-				run_test(el)
+				run_test(el, file_name)
 
 
 if __name__=="__main__":
